4f9cb16fe614d3e3759c1cd46292271dba1a9303
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-09-08 02:42:49 +0900

    Update middleware.py

--- project/middleware.py
+++ project/middleware.py
@@ -5,20 +5,21 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from urllib.parse import parse_qs
 
-User = get_user_model()
 
 @database_sync_to_async
 def get_user_from_token(token):
     try:
+
+        User = get_user_model()
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
         user = User.objects.get(id=payload["user_id"])
         return user
     except Exception:
         return None
 
+
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-
         query_string = parse_qs(scope["query_string"].decode())
         token = query_string.get("token")
 
