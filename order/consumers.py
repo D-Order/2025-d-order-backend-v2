f7eba827c5fab53f08ac5e61b9a3e539bdfb5c1f
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-09-09 02:47:00 +0900

    Update consumers.py

--- order/consumers.py
+++ order/consumers.py
@@ -1,6 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
+from asgiref.sync import sync_to_async
 from django.utils import timezone
 from datetime import timedelta
 
