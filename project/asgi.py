317a70c658955c3dd32fb32cbfce047a6b56d0e2
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-09-09 12:03:23 +0900

    Update asgi.py

--- project/asgi.py
+++ project/asgi.py
@@ -1,13 +1,14 @@
 import os
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from project.middleware import JWTAuthMiddleware
 import project.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
