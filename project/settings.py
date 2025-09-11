1140fc982716c2582a217c3b3c0c0478c53dd80b
hyunwoo <dlagusdn0218@gmail.com>
2025-09-12 01:45:55 +0900

    Chore: 5174 포트 열기

--- project/settings.py
+++ project/settings.py
@@ -242,9 +242,13 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
     'https://localhost:5173',
+    'http://localhost:5174',
+    'https://localhost:5174',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
     'https://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
+    'https://127.0.0.1:5174',
 
     #백엔드 배포주소
     "https://d-order.store", 
