e8626b35392fe33e1eb764e6e4cad0dd7dfcadad
hyunwoo <dlagusdn0218@gmail.com>
2025-09-19 02:10:28 +0900

    Fix: 쿠폰 발급 개수 조회

--- coupon/models.py
+++ coupon/models.py
@@ -8,6 +8,7 @@ class Coupon(models.Model):
     discount_type = models.CharField(max_length=20)
     discount_value = models.FloatField()
     quantity = models.IntegerField()
+    initial_quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
